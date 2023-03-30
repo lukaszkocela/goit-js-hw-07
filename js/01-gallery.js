f8ddcba6a13927f9a1b85074e024ed2142dbb4d0
Łukasz Kocela <117775643+lukaszkocela@users.noreply.github.com>
2023-03-30 10:15:47 +0200

    Add files via upload
    solution added

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const newImage = `<div class="gallery__item"> <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a></div>`;
+  galleryEl.insertAdjacentHTML("beforeend", newImage);
+}
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", (event) => {
+          if (event.key === "Escape") {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+  instance.show();
+});
---
+++ js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const newImage = `<li class="gallery__item"><a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a></li>`;
+  galleryEl.insertAdjacentHTML("beforeend", newImage);
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
